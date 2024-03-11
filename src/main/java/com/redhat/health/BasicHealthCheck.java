ae60f886bb1ffcd65f7d48eea4807efa2cccc982
Donato Marrazzo <donato.marrazzo@gmail.com>
2024-03-11 20:21:49 +0100

    health check fixed

--- src/main/java/com/redhat/health/BasicHealthCheck.java
+++ src/main/java/com/redhat/health/BasicHealthCheck.java
@@ -1,5 +1,12 @@
 package com.redhat.health;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
@@ -15,6 +22,15 @@ import jakarta.inject.Inject;
 @ApplicationScoped
 public class BasicHealthCheck implements HealthCheck {
 
+    @ConfigProperty(name = "model.server.host", defaultValue = "modelmesh-serving")
+    private String modelserverHost;
+
+    @ConfigProperty(name = "model.server.port", defaultValue = "8008")
+    private int modelserverPort;
+
+    @ConfigProperty(name = "model.name", defaultValue = "fraud")
+    private String modelName;
+
     @Inject
     @RestClient
     DefaultApi infer;
@@ -24,10 +40,20 @@ public class BasicHealthCheck implements HealthCheck {
         HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Model Serving connection health check");
 
         try {
-            infer.getV2HealthReady();
-            responseBuilder.up();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + modelserverHost + ":" + modelserverPort + "/v2/models/" + modelName))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+            if (response.statusCode() == 200)
+                responseBuilder.up();
+            else
+                responseBuilder.down();
         } catch (Exception e) {
             responseBuilder.down();
+            responseBuilder.withData("exception", e.toString());
         }
 
         return responseBuilder.build();
